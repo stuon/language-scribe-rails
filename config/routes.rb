317586e93dfee79fb40a5f41155a2a4762bd698f
stuon <santuon@yahoo.com>
2017-06-16 23:26:38 +1000

    added email confirmation, and fixed home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'pages/home', to: "pages#home"
+  get 'pages/about', to: 'pages#about' 
+ 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   root 'pages#home'
-   
+  root 'pages#home'
+
 end
