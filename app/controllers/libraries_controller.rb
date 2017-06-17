f2f8245bc62496554df3d54ab1a58086d08f6fa5
stuon <santuon@yahoo.com>
2017-06-18 00:14:33 +1000

    added admin to user

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -54,7 +54,7 @@ class LibrariesController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @library.user
+        if current_user != @library.user and !current_user.admin?
             flash[:danger] = "You can only edit or delete your own libraries"
             redirect_to root_path
         end
