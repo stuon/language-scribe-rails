c4e4387c08cc31b4ccb4175cdbd736445ea4571e
stuon <santuon@yahoo.com>
2017-06-17 23:48:43 +1000

    Added unique name on library name

--- app/models/library.rb
+++ app/models/library.rb
@@ -3,5 +3,6 @@ class Library < ActiveRecord::Base
     validates :name, presence: true, length: { minimum: 3, maximum: 25}
     validates :description, presence: false, length: { maximum: 120 }    
     validates :user_id, presence: true
+    validates_uniqueness_of :name, case_sensitive: false, scope: :user_id
 end
 
